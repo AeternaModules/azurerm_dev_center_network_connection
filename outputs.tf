15b4839c3af4c1283f59d646e7d41ed053e5e171
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:45:58 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "dev_center_network_connections" {
+  description = "All dev_center_network_connection resources"
+  value       = azurerm_dev_center_network_connection.dev_center_network_connections
+}
+
