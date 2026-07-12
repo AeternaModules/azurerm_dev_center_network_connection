8527f66ea720bb78bc2766acf930efd882d2a721
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:58:18 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+data "azurerm_key_vault_secret" "domain_password" {
+  for_each     = { for k, v in var.dev_center_network_connections : k => v if v.domain_password_key_vault_id != null && v.domain_password_key_vault_secret_name != null }
+  name         = each.value.domain_password_key_vault_secret_name
+  key_vault_id = each.value.domain_password_key_vault_id
+}
 resource "azurerm_dev_center_network_connection" "dev_center_network_connections" {
   for_each = var.dev_center_network_connections
 
@@ -7,7 +12,7 @@ resource "azurerm_dev_center_network_connection" "dev_center_network_connections
   resource_group_name = each.value.resource_group_name
   subnet_id           = each.value.subnet_id
   domain_name         = each.value.domain_name
-  domain_password     = each.value.domain_password
+  domain_password     = each.value.domain_password != null ? each.value.domain_password : try(data.azurerm_key_vault_secret.domain_password[each.key].value, null)
   domain_username     = each.value.domain_username
   organization_unit   = each.value.organization_unit
   tags                = each.value.tags
--- variables.tf
+++ variables.tf
@@ -10,22 +10,26 @@ Required:
 Optional:
     - domain_name
     - domain_password
+    - domain_password_key_vault_id (alternative to domain_password - read from Key Vault instead)
+    - domain_password_key_vault_secret_name (alternative to domain_password - read from Key Vault instead)
     - domain_username
     - organization_unit
     - tags
 EOT
 
   type = map(object({
-    domain_join_type    = string
-    location            = string
-    name                = string
-    resource_group_name = string
-    subnet_id           = string
-    domain_name         = optional(string)
-    domain_password     = optional(string)
-    domain_username     = optional(string)
-    organization_unit   = optional(string)
-    tags                = optional(map(string))
+    domain_join_type                      = string
+    location                              = string
+    name                                  = string
+    resource_group_name                   = string
+    subnet_id                             = string
+    domain_name                           = optional(string)
+    domain_password                       = optional(string)
+    domain_password_key_vault_id          = optional(string)
+    domain_password_key_vault_secret_name = optional(string)
+    domain_username                       = optional(string)
+    organization_unit                     = optional(string)
+    tags                                  = optional(map(string))
   }))
 }
 
